833315163dca31c467b951841a524893dff068d3
Zaratrasegit <122055075+Zaratrasegit@users.noreply.github.com>
2023-06-15 20:56:32 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,20 @@
+@echo off
+
+
+@REM Installation des librairies pystyle et sympy
+pip install pystyle
+pip install sympy
+
+@REM Verification de l'installation
+pip show pystyle
+pip show sumpy
+echo Installation terminée.
+
+
+python equation.py
+
+
+
+
+
+
